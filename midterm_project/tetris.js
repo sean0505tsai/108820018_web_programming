b5269b2613c9a201153d7e08375e224706ebd168
sean0505tsai <t108820018@ntut.org.tw>
2022-11-23 14:41:31 +0800

    building UI

---
+++ midterm_project/tetris.js
@@ -0,0 +1,29 @@
+var gameArea
+
+function onLoad(){
+
+}
+
+function play(){
+
+}
+
+function moveLeft(){
+
+}
+
+function moveRight(){
+
+}
+
+function moveDown(){
+
+}
+
+function rotate(){      // rotate clockwise
+
+}
+
+function reset(){
+    console.log("reset");
+}
