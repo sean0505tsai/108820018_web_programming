1efe23f599e781ecc08ed3327de9a1b42db81593
sean0505tsai <t108820018@ntut.org.tw>
2022-11-30 19:04:39 +0800

    Update App.js

--- lab_react_01/src/App.js
+++ lab_react_01/src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 import Title from './components/title';
 
 function App() {
-  return (
-    <div className="App">
-      <Title/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello class!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Title />
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Hello class!
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
