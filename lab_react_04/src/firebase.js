cf26a953a82aac41b11ed25d8c7d285bf57b8b00
sean0505tsai <t108820018@ntut.org.tw>
2022-12-14 19:34:26 +0800

    deployed on firebase

--- lab_react_04/src/firebase.js
+++ lab_react_04/src/firebase.js
@@ -5,13 +5,14 @@ import { getDatabase } from "firebase/database"
 const firebaseConfig = {
 	apiKey: "AIzaSyC6tn7GghhuQArWgDaefvvbmC9tow1sXQE",
 	authDomain: "ntut-web-20221214-108820018.firebaseapp.com",
+	databaseURL: "https://ntut-web-20221214-108820018-default-rtdb.asia-southeast1.firebasedatabase.app",
 	projectId: "ntut-web-20221214-108820018",
 	storageBucket: "ntut-web-20221214-108820018.appspot.com",
 	messagingSenderId: "1048651986796",
 	appId: "1:1048651986796:web:9fddbea526e7a230246549"
-};
-
-export const database = getDatabase(app);
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const database = getDatabase(app);
